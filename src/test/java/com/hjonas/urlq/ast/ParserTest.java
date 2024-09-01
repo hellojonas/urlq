9577ebbb55a7c2e7f16619f5be25d63851684b70
Hello Jonas <hellojonashp@gmail.com>
2024-09-01 15:37:18 +0200

    add parser & lexer test structure

---
+++ src/test/java/com/hjonas/urlq/ast/ParserTest.java
@@ -0,0 +1,13 @@
+package com.hjonas.urlq.ast;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class ParserTest {
+
+    @Test
+    public void shouldParseSucessfully() {
+    }
+}
